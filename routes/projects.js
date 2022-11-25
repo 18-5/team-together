3c306be922fc6553c0fa199d87a4a09eb7353bbb
minje0312 <phone7101@naver.com>
2022-11-26 01:44:40 +0900

    first step about project api complete

--- routes/projects.js
+++ routes/projects.js
@@ -86,7 +86,7 @@ router.get('/', (req, res) => {
 
 // :project-id 프로젝트
 router.get('/:projectId', (req, res) => {
-    let { projectId } = req.params;
+    let projectId = req.params['projectId'];
     console.log(projectId);
 
     connection.query(
@@ -100,7 +100,7 @@ router.get('/:projectId', (req, res) => {
 
 // :project-id 프로젝트 수정
 router.put('/:projectId', (req, res) => {
-    let { projectId } = req.params;
+    let projectId = req.params['projectId'];
     console.log(projectId);
     let name = req.body.projectName;
     console.log(name);
@@ -127,10 +127,14 @@ router.put('/:projectId', (req, res) => {
 });
 
 // 프로젝트 멤버
-// *진행 중*
 router.get('/:projectId/members', (req, res) => {
+    let projectId = req.params['projectId'];
+    console.log(projectId);
+
+    let sql = "SELECT * FROM user WHERE userId IN "
+        + "(SELECT userId FROM member WHERE projectId=" + projectId + ");";
     connection.query(
-        "SELECT * FROM project;", 
+        sql, 
         (err, rows, fields) => {
             console.log(rows);
             res.send(rows);
@@ -139,10 +143,15 @@ router.get('/:projectId/members', (req, res) => {
 });
 
 // 프로젝트 리더
-// *진행 중*
 router.get('/:projectId/members/leader', (req, res) => {
+    let projectId = req.params['projectId'];
+    console.log(projectId);
+
+    let sql = "SELECT * FROM user WHERE userId IN "
+        + "(SELECT userId FROM member WHERE projectId=" + projectId + " AND "
+        + "admin=1);";
     connection.query(
-        "SELECT * FROM project;", 
+        sql, 
         (err, rows, fields) => {
             console.log(rows);
             res.send(rows);
@@ -151,10 +160,14 @@ router.get('/:projectId/members/leader', (req, res) => {
 });
 
 // 모든 프로젝트 지원자
-// *진행 중*
 router.get('/:projectId/applicants', (req, res) => {
+    let projectId = req.params['projectId'];
+    console.log(projectId);
+
+    let sql = "SELECT * FROM user WHERE userId IN "
+        + "(SELECT userId FROM applicant WHERE projectId=" + projectId + ");";
     connection.query(
-        "SELECT * FROM project;", 
+        sql, 
         (err, rows, fields) => {
             console.log(rows);
             res.send(rows);
@@ -163,25 +176,50 @@ router.get('/:projectId/applicants', (req, res) => {
 });
 
 // 프로젝트 지원자 생성
-// *진행 중*
+// userId는 body로 받아오는 것으로 가정
 router.post('/:projectId/applicants', (req, res) => {
-    connection.query(
-        "SELECT * FROM project;", 
-        (err, rows, fields) => {
-            console.log(rows);
-            res.send(rows);
+    let userId = req.body.userId;
+    console.log(userId);
+    let sql = "SELECT COUNT(*) FROM user WHERE userId=" + userId + ";";
+    connection.query(sql, (err, rows, fields) => {
+        if(rows != 0){
+            sql = "INSERT INTO applicant VALUES (?, ?);";
+            let projectId = req.params['projectId'];
+            console.log(projectId);
+        
+            let params = [projectId, userId];
+            connection.query(sql, params, 
+                (err, rows, fields) => {
+                    console.log(rows);
+                    res.send(rows);
+                });
         }
-    );
+        else{
+            console.log("Not Existing User")
+            res.send("Not Existing User");
+        }
+    })
 });
 
 // 프로젝트 지원자 삭제
-// *진행 중*
-router.delete('/:projectId/applicants/:user-id', (req, res) => {
+router.delete('/:projectId/applicants/:userId', (req, res) => {
+    let projectId = req.params['projectId'];
+    let userId = req.params['userId'];
+    console.log(projectId);
+    console.log(userId);
+
+    let sql = "DELETE FROM applicant WHERE projectId=" + projectId
+        + " AND userId=" + userId + ";";
+
     connection.query(
-        "SELECT * FROM project;", 
+        sql, 
         (err, rows, fields) => {
-            console.log(rows);
-            res.send(rows);
+            if(err){
+                console.log("delete fail");
+                res.send("delete fail");    
+            }
+            console.log("delete success");
+            res.send("delete success");
         }
     );
 });
