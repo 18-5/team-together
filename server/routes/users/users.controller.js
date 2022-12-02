b59512f79845973ba71957e1fd3c6b468a3c5bd7
DrRivaski <48974215+DrRivaski@users.noreply.github.com>
2022-12-02 20:11:11 +0900

    쪽지보내기 구현

--- server/routes/users/users.controller.js
+++ server/routes/users/users.controller.js
@@ -2,6 +2,8 @@ const dotenv = require("dotenv").config({ path: "../.env" });
 const mysql = require('mysql');
 const { serialize } = require('v8');
 
+//console.log(process.env.DATABASE_HOST);
+
 const connection = mysql.createConnection({
     host: process.env.DATABASE_HOST, 
     user: process.env.DATABASE_USER,
@@ -41,10 +43,13 @@ exports.signIn = (req, res) => {
 
 
 // 내가 리더인 프로젝트
-// *진행 중*
+// *complete*
 exports.whereUserIsLeader = (req, res) => {
+    let userId = req.params['userId'];
+    let sql = "SELECT * FROM project WHERE projectId IN (SELECT projectId FROM member WHERE userId = " + userId + " AND leader = 1);";
+
     connection.query(
-        "SELECT * FROM member WHERE leader = 1;",
+        sql,
         (err, rows, fields) => {
             res.send(rows);
         }
@@ -55,7 +60,7 @@ exports.whereUserIsLeader = (req, res) => {
 // *complete*
 exports.userProfile = (req, res) => {
     let userId = req.params['userId'];
-    console.log(userId);
+    console.log(userName);
 
     connection.query(
         "SELECT * FROM user WHERE userId =" + userId + ";",
@@ -167,6 +172,7 @@ exports.userRecievedMsg = (req, res) => {
 }
 
 // 회원의 모든 쪽지
+// 카톡방 리스트
 // *complete*
 exports.userAllMsg = (req, res) => {
     let userId = req.params['userId'];
@@ -189,13 +195,19 @@ exports.userAllMsg = (req, res) => {
 }
 
 // 회원의 특정 쪽지
+// 특정 카톡방 들어가면 나오는 쪽지 리스트
+// ORDER BY 수정 필요
 // *진행 중*
 exports.userMsgByMsgId = (req, res) => {
+    let userId = req.params['userId'];
+    let otherId = req.params['otherId'];
+
+    let sql = "SELECT * FROM message WHERE senderId = " + userId + " OR senderId = " + otherId + " OR receiverId = " + userId + " OR receiverId = " + otherId + " ORDER BY createdAt DESC;";
     
     //클릭하면 sender, receiver 동시에 겹치는거 싹 긁어서 뿌리기
     //sql paging
     connection.query(
-        "SELECT * FROM user;",
+        sql,
         (err, rows, fields) => {
             if(err){
                 res.send(err);
@@ -209,8 +221,10 @@ exports.userMsgByMsgId = (req, res) => {
 }
 
 // 회원의 특정 쪽지 삭제
+// 쪽지 하나만 삭제? 대화방 전체 삭제? 나에게만 삭제? 둘 다에게 삭제?
 // *진행 중*
 exports.deleteMsg = (req, res) => {
+    
     connection.query(
         "SELECT * FROM user;",
         (err, rows, fields) => {
@@ -221,13 +235,36 @@ exports.deleteMsg = (req, res) => {
 }
 
 // 회원에게 쪽지 보내기
-// *진행 중*
+// senderId와 receiverId 모두 form으로 post, 화면에서는 두 아이디 모두 안보이도록 숨기기
+// messageId는 random 4자리수 * 4자리수
+// *complete*
 exports.sendMsg = (req, res) => {
+    let senderId = req.body.senderId;
+    let receiverId = req.body.receiverId;
+    let content = req.body.content;
+    let messageId = Math.ceil(Math.random() * 10000) * Math.ceil(Math.random() * 10000);
+
+    let sql = "INSERT INTO message VALUES (?, ?, ?, ?, ?, 1, NULL);";
+    let params = [senderId, receiverId, messageId, content, new Date().toISOString().slice(0, 19).replace('T', ' ')];
+
+    /*connection.query(
+        "SELECT COUNT(*) AS count FROM user WHERE userId = " + senderId + " OR userId = " + receiverId + ";",
+        (err, rows, fields) => {
+            const cnt = JSON.stringify(rows[0].count);
+            if(err) {
+                res.send(err);
+            } else if (cnt === "2") {
+                res.send(cnt);
+            } else {
+                res.send("receiver or sender가 회원이 아닙니다.");
+            }
+        }
+    )*/
     connection.query(
-        "SELECT * FROM user;",
+        sql, params,
         (err, rows, fields) => {
             if (err) {
-
+                res.send(err);
             }
             else {
                 console.log(rows);
